abb6d2020c2b831260d20afb24cb591029b730bb
Jeffrey Ausborn <jeffrey.ausborn@walmart.com>
2023-11-03 10:15:10 -0700

    need to test on windows

--- main.go
+++ main.go
@@ -4,13 +4,15 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"log"
 	"os"
 	"time"
 
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
+	fyne "fyne.io/fyne"
+	fa "fyne.io/fyne/app"
+	"fyne.io/fyne/container"
+	"fyne.io/fyne/dialog"
+	"fyne.io/fyne/widget"
 )
 
 const (
@@ -18,31 +20,54 @@ const (
 )
 
 var (
-	daocLogs DaocLogs
+	daocLogs    DaocLogs
+	chatLogFile string
 )
 
+func readChatFile(r fyne.URIReadCloser, err error) {
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(r.URI())
+}
+
 func main() {
+	logPath := flag.String("file", "", "Path to chat.log")
+	streamLogs := flag.Bool("stream", false, "")
+	flag.Parse()
+	if *logPath == "" {
+		log.Fatal("Requied argument missing: --file path/to/chat.log")
+	}
 	daocLogs = DaocLogs{}
-	a := app.New()
-	w := a.NewWindow("Dark Age of Camelot - Chat Parser")
+	a := fa.New()
+	w := a.NewWindow("Dark Age of Camelot - Chat Parser - By: Theorist")
 	damageLabel := widget.NewLabel("")
-	w.SetContent(container.NewVBox(damageLabel))
-	go func() {
-		logPath := flag.String("file", "", "Path to chat.log")
-		streamLogs := flag.Bool("stream", false, "")
-		flag.Parse()
-		if *logPath != "" {
-			openLogFile(*logPath)
-			damageLabel.SetText(daocLogs.writeLogValues())
-			if *streamLogs {
-				for range time.Tick(time.Second * LOG_STREAM_TIME) {
-					daocLogs = DaocLogs{}
-					openLogFile(*logPath)
-					damageLabel.SetText(daocLogs.writeLogValues())
-				}
+
+	w.SetContent(container.NewVBox(
+		damageLabel,
+		widget.NewButton("Load Chat Log", func() {
+			fd := dialog.NewFileOpen(func(fyne.URIReadCloser, error) {
+			}, w)
+			fd.Show()
+		}),
+		widget.NewButton("Refresh", func() {
+			e := os.Remove(*logPath)
+			if e != nil {
+				log.Fatal(e)
 			}
-		} else {
-			fmt.Println("Requied argument missing: --file path/to/chat.log")
+		}),
+	))
+
+	go func() {
+		openLogFile(*logPath)
+		damageLabel.SetText(daocLogs.writeLogValues())
+		if *streamLogs {
+			for range time.Tick(time.Second * LOG_STREAM_TIME) {
+				daocLogs = DaocLogs{}
+				openLogFile(*logPath)
+				damageLabel.SetText(daocLogs.writeLogValues())
+			}
 		}
 	}()
 
